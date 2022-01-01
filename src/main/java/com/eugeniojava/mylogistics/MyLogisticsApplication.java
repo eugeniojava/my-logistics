65a6f0c5c06a4679c82533ce9fe4ad2f85653f52
Eugênio Moreira <eugeniojava@outlook.com>
2022-01-01 17:37:52 -0300

    initial commit

---
+++ src/main/java/com/eugeniojava/mylogistics/MyLogisticsApplication.java
@@ -0,0 +1,11 @@
+package com.eugeniojava.mylogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLogisticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyLogisticsApplication.class, args);
+    }
+}
